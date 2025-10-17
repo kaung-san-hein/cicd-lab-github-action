65043448d83cfd0b218feb0ab4c78bfc16de1a3f
kaung-san-hein <kaungsanhein2019@gmail.com>
2025-10-17 20:03:56 +0900

    add pm2 setup

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps: [
+      {
+        name: 'cicd-lab',
+        script: 'npm',
+        args: 'start -p 3000',
+        exec_mode: 'cluster', // Use 'cluster' for horizontal scaling, 'fork' for single instance
+        instances: 1,
+        watch: true, // Set to true to enable watching for file changes and automatic restarts
+        env: {
+          NODE_ENV: 'production',
+        },
+      },
+    ],
+  };
