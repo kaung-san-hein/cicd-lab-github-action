d799229324dbccf9058a121d3cc648c6935eafb4
kaung-san-hein <kaungsanhein2019@gmail.com>
2025-10-17 23:27:13 +0900

    change port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
       {
         name: 'cicd-lab',
         script: 'npm',
-        args: 'start -p 3000',
+        args: 'start -p 4000',
         exec_mode: 'cluster', // Use 'cluster' for horizontal scaling, 'fork' for single instance
         instances: 1,
         watch: true, // Set to true to enable watching for file changes and automatic restarts
